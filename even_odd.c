3d5726702846979bd69e719d2a3462da86cae71c
Aishwarya Mhetre <mhetreaishwarya07@gmail.com>
2024-04-17 17:24:47 +0530

    commit

---
+++ even_odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() 
+{
+int i;
+ printf ("Enter an element :");
+ scanf("%d",&i);
+if(i%2==0)
+{
+    printf("\n%d is even number",i);
+}
+else 
+{
+    printf("%d is odd number",i);
+return 0;
+}
+}
